4a242ee1d6fc8a869f271e114ada619bd7d78b38
Pierre Berkovits <pierre.berkovits@netplus>
2023-10-06 21:31:21 +0200

    home-title

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,14 +3,8 @@ import homeimg from '../../assets/home.png'
 function Home() {
     return (
         <div className='home-main'>
-            <div className="home-title">
-                <div className="home-image">
-                <img src={homeimg} alt="home" />
-                </div>
-                <div className="home-p">
-                <p>Chez vous, partout et ailleurs</p>
-                </div>
-            </div>
+            <img src={homeimg} alt="home" className="home-image" />
+            <p className="home-p">Chez vous, partout et ailleurs</p>
         </div>
     )
 }
