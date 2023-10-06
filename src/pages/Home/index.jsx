b66b67060903f1104be326ab5401bfe47065a5df
Pierre Berkovits <pierre.berkovits@netplus>
2023-10-06 13:18:56 +0200

    header img +

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,10 @@
-// import '../styles/home/_home.scss'
+import homeimg from '../../assets/home.png'
 
 function Home() {
     return (
         <div className='home-main'>
             <div className="home-title">
-                <img src="../../assets/home.png" alt="home" className="home-image"/>
+                <img src={homeimg} alt="home" className="home-image"/>
                 <p className="home-p">Chez vous, partout et ailleurs</p>
             </div>
         </div>
