a84d39f5c35f492d766c14d2bec49965838c0238
Pierre Berkovits <pierre.berkovits@netplus>
2023-10-06 12:01:05 +0200

    header img

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,6 +3,10 @@
 function Home() {
     return (
         <div className='home-main'>
+            <div className="home-title">
+                <img src="../../assets/home.png" alt="home" className="home-image"/>
+                <p className="home-p">Chez vous, partout et ailleurs</p>
+            </div>
         </div>
     )
 }
