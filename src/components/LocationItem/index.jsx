731e680e966007deeb3c3f4f01d92f158c537717
Pierre Berkovits <pierre.berkovits@netplus>
2023-10-07 18:01:59 +0200

    import json

---
+++ src/components/LocationItem/index.jsx
@@ -0,0 +1,10 @@
+function LocationItem({id,title}) {
+    return (
+        <li className="home-gale">
+            <div className="home-gale__rect">
+                <p className="home-gale__p">{title}</p>
+            </div>
+        </li>
+    )
+}
+export default LocationItem
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,23 @@
+import React from 'react';
+import locationList from '../../data/vingtannonces.json'
 import homeimg from '../../assets/home.png'
+import LocationItem from '../../components/LocationItem'
 
 function Home() {
     return (
+        <React.Fragment>
         <div className='home-main'>
             <img src={homeimg} alt="home" className="home-image" />
             <p className="home-p">Chez vous, partout et ailleurs</p>
         </div>
+        <ul className='home-loclist'>
+            {locationList.map(({id, title}) =>
+                (<div key={id}>
+                    <LocationItem title ={title} />
+                </div>
+            ))}
+        </ul>
+        </React.Fragment>
     )
 }
 export default Home
