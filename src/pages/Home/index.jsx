c477107c6ddd42bfd356b5fef5074759bd5cde0f
Pierre Berkovits <pierre.berkovits@netplus>
2023-10-10 22:10:43 +0200

    sass+

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,51 +2,46 @@ import React from 'react';
 import {useState} from 'react'
 import locationList from '../../data/vingtannonces.json'
 import homeimg from '../../assets/home.png'
+import LeftArrow from '../../components/LeftArrow'
+import RightArrow from '../../components/RightArrow'
 import LocationItem from '../../components/LocationItem'
 
 
 function Home() {
-/*     const [filtL, setfiltL] = useState()
-    const filtR = locationList.filter(locL => locL.title.includes('Studio'))
-    console.log(filtR.length)
-    setfiltL(filtR) */
+    const locLLen = locationList.length
+    const [curI, setcurI] = useState(0)
+    var curM = 6
+    function nextGal(curI) {
+        ((curI + 6) < locLLen ) ? setcurI(curI + 6) : curI = 0
+        curM = curI + 6
+    }
+    function prevGal(curI) {
+        ((curI - 6) >= 0 ) ? setcurI(curI - 6) : curI = (locLLen - 7)
+        curM = curI + 6
+    }
     return (
         <React.Fragment>
         <div className='home-main'>
             <img src={homeimg} alt="home" className="home-image" />
             <p className="home-p">Chez vous, partout et ailleurs</p>
             <div className='gale'>
+                <div>
+                    <LeftArrow/>
+                </div>
                 <ul  className='gale-ul'>
-                {locationList.filter((locL, index) => index < 6).map(
+                {locationList.filter((locL, index) => (index >= curI && index < curM )).map(
                     ({id,title}) =>
                     (<div key= {id}>
                         <LocationItem title={title} />
                     </div>)
                 )}
                 </ul>
+                <div>
+                    <RightArrow/>
+                </div>
             </div>
         </div>
         </React.Fragment>
     )
 }
 export default Home
-/* {locationList.map(({id, title}) =>
-(<div key={id}>
-    <LocationItem title={title} />
-</div>
-))} */
-{/* <React.Fragment>
-<div className='home-main'>
-    <img src={homeimg} alt="home" className="home-image" />
-    <p className="home-p">Chez vous, partout et ailleurs</p>
-    <div className='gale'>
-        <ul className='gale-ul'>
-            {locationList.map(({id, title}) =>
-                (<li key={id} className='gale-ul__li'>
-                    <p className='gale-ul__li-p'>{title}</p>
-                </li>
-            ))}
-        </ul>
-    </div>
-</div>
-</React.Fragment> */}
