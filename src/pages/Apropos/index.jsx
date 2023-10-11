f26b580869ded506097e298975ad025ffbfa9452
Pierre Berkovits <pierre.berkovits@netplus>
2023-10-11 15:23:30 +0200

    collaps

--- src/pages/Apropos/index.jsx
+++ src/pages/Apropos/index.jsx
@@ -1,6 +1,21 @@
 import React from 'react'
+import Banner from '../../components/Banner'
+import Collapse from '../../components/Collapse'
+import apropimg from '../../assets/Aprop.png'
 
 function Apropos() {
-  return <div>A propos</div>
+  return (
+    <React.Fragment>
+      <div>
+        <Banner imag={apropimg} />
+      </div>
+      <div>
+        <Collapse />
+        {/* <Collapse />
+        <Collapse />
+        <Collapse /> */}
+      </div>
+    </React.Fragment>
+  )
 }
 export default Apropos
